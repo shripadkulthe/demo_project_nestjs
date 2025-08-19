b5b3870997e959221b79dc99fab1538ce5eaa382
shripad <shripad.kulthe@updatusinc.com>
2025-08-19 23:30:06 +0530

    Added middleware in app module to check API validation

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,20 @@
-import { Module } from '@nestjs/common';
+import { MiddlewareConsumer, Module, NestModule, RequestMethod } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ProductsController } from './products/products.controller';
 import { ProductsService } from './products/products.service';
 import { UserModule } from './user/user.module';
+import { ApiTokenCheckMiddleware } from './common/middleware/api-token-check.middleware';
 
 @Module({
   imports: [UserModule],
   controllers: [AppController, ProductsController],
   providers: [AppService, ProductsService],
 })
-export class AppModule {}
+export class AppModule implements NestModule{
+  configure(consumer: MiddlewareConsumer) {
+    
+    consumer.apply(ApiTokenCheckMiddleware)
+      .forRoutes({path: '*', method: RequestMethod.ALL});
+  }
+}
